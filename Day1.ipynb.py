53dc818aa0010148252c5085556d68a00f54cac8
mawukojonathan <mawukojonathan751@gmail.com>
2023-08-22 16:57:13 +0000

    first commit

---
+++ Day1.ipynb.py
@@ -0,0 +1,23 @@
+# string manipulation
+
+print("hello \n hello \n hello \n hello")
+print("\n")
+
+#interactive code exercise (input function)
+
+name = input("what is your name? ")
+print(len(name))
+print("\n")
+# soloution
+
+print(len(input("what is your name? ")))
+
+#Day 1 project band name generator
+
+print("Welcome to the Bnad Name Generator")
+city = input("Which city did you grow up in? \n ")
+pet = input("What is the name of your pet?  \n")
+print("Your Band Name could be "  + city +" "  + pet)
+
+#notes
+#We can find string length in Python using the built-in len() function. Let’s look at how this function works, and also let’s try finding the length of various types of Python strings using len().
